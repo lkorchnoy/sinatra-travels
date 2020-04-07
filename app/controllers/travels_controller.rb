75288b3d276be94fe00f56aab097a04c94ff42fa
lkorchnoy <lkorchnoy@yahoo.com>
2020-04-07 15:43:14 -0400

    user logout, user show page updated, travels controller, travels new and show views, helpers class and addes helpers methods to controller

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,5 +1,34 @@
 class TravelsController < ApplicationController 
 
+get '/travels/new' do
+    if !Helpers.is_logged_in?(session) 
+        redirect '/'
+    end
+    erb :'travels/new'
+end
+
+post '/travels' do
+    #if Helpers.current_user(session).id == Travel.find_by(id: params[:id]).user_id
+    travel = Travel.create(params)
+    user = Helpers.current_user(session)
+    travel.user = user 
+    travel.save
+    redirect to "/users/#{user.id}"
+end
+
+get '/travels/:id' do 
+    if !Helpers.is_logged_in?(session) 
+        redirect '/'
+    end
+    @travel = Travel.find_by(id: params[:id])
+    if !@travel 
+        redirect to '/'  #travels
+    end
+
+    erb :'travels/show'
+end
+
+
     
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,40 @@
 class UsersController < ApplicationController 
 
+get '/users' do 
+    if Helpers.is_logged_in?(session)
+      @users = User.all 
+    else    
+      redirect to '/'
+    end
+    erb :'users/index' 
+end
+
+
+
+
 get '/signup' do
+    if Helpers.is_logged_in?(session)
+      user = Helpers.current_user(session)
+      redirect to "/users/#{user.id}"
+    end
     erb :'users/signup'
 end
 
 get '/login' do 
+    if Helpers.is_logged_in?(session)
+      user = Helpers.current_user(session)
+      redirect to "/users/#{user.id}"
+    end
     erb :'users/login'
 end
 
 post '/login' do 
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-        session[:user_id] = user.id 
-        redirect to "/users/#{user.id}"
+      session[:user_id] = user.id 
+      redirect to "/users/#{user.id}"
     else     
-        redirect to '/signup'
+      redirect to '/signup'
     end
 end
 
@@ -29,12 +49,18 @@ post '/signup' do
   end
 
 get '/users/:id' do 
-    if User.find_by(id: params[:id])
-    @user = User.find_by(id: params[:id])
+    if Helpers.is_logged_in?(session) && User.find_by(id: params[:id])
+      @user = User.find_by(id: params[:id])
+      @travels = @user.travels 
     else 
         redirect to '/'
     end
     erb :'users/show'
+end
+
+get '/logout' do 
+    session.clear
+    redirect to '/'
 end
 
 end
